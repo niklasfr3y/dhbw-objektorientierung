2acc9756b440f1bbe322aea9876d672807c50f1d
paulus.marvin <paulus.marvin@348A26>
2019-11-14 16:14:52 +0100

    first real coding commit

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -38,8 +38,9 @@ class GameWindow : public Gosu::Window
 	Gosu::Image cherryLady;
 	Gosu::Image barBarBar;
 	Gosu::Image orange;
+	Gosu::Image background;
 
-	//map<Symbols, Gosu::Image> translate;
+	map<Symbols, Gosu::Image> translate;
 
 	int y_Seven = 0;
 	int y_Melon = 0;
@@ -48,6 +49,7 @@ public:
 	GameWindow()
 		: Window(1600, 1000), seven("DIE SIEBEN DU HUND.png"), melon("MELOOOONE.png"), plum("IT'S  A MOTHERFUCKIN' PLUM.png"), zitrapattoni("ZITR(APATT)ONI.png")
 		, banana("BANANANAANAS.png"), bIGWIN("JAAACKPOOOOOT.png"), cherryLady("JUICYCHERRY.png"), barBarBar("ES REGNET BARES BITCHES.png"), orange("ORANGEMORANGE BLYAT.png")
+		, background("Slotti.png")
 
 
 	{
@@ -59,12 +61,13 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override
 	{
-		graphics().draw_rect(0, 0, 1600, 100, Gosu::Color::BLUE, 1.0);
+		background.draw(0, 0, 1.0);
+		/*graphics().draw_rect(0, 0, 1600, 100, Gosu::Color::BLUE, 1.0);
 		graphics().draw_rect(0, 1000 - 100, 1600, 100, Gosu::Color::BLUE, 1.0); 
 		graphics().draw_rect(0, 0, 300, 1000, Gosu::Color::BLUE, 1.0);
-		graphics().draw_rect(1600 - 300, 0, 300, 1000, Gosu::Color::BLUE, 1.0);
+		graphics().draw_rect(1600 - 300, 0, 300, 1000, Gosu::Color::BLUE, 1.0);*/
 		seven.draw(300, y_Seven, 0.0);
-		graphics().draw_rect(488, 0, 12, 1000, Gosu::Color::BLUE, 1.0);
+		//graphics().draw_rect(488, 0, 12, 1000, Gosu::Color::BLUE, 1.0);
 		melon.draw(500, y_Melon, 0.0);
 
 	}
